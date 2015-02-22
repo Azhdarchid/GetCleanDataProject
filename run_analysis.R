890e7224f22d62d66d5dafcd59a1f04e040cd949
Des <dlteter@gmail.com>
2015-02-21 20:50:52 -0600

    first commit

---
+++ run_analysis.R
@@ -0,0 +1,38 @@
+# Load requirements
+library(plyr)
+
+# Read all relevant data
+xTestData <- read.table("test/X_test.txt")
+yTestData <- read.table("test/y_test.txt")
+subjectTestData <- read.table("test/subject_test.txt")
+xTrainData <- read.table("train/X_train.txt")
+yTrainData <- read.table("train/y_train.txt")
+subjectTrainData <- read.table("train/subject_train.txt")
+
+# Merge data sets
+xDataMerged <- rbind(xTestData, xTrainData)
+yDataMerged <- rbind(yTestData, yTrainData)
+subjectDataMerged <- rbind(subjectTestData, subjectTrainData)
+
+# Correct all column names
+meanSTD <- read.table("features.txt")[,2]
+names(xDataMerged) = meanSTD
+names(yDataMerged) <- "Activity"
+names(subjectDataMerged) = "Subject"
+
+# Extract measurements for xData
+extractor <- grepl("mean|std", meanSTD)
+xDataMeasures <- xDataMerged[, extractor]
+
+# Correct ydata activity labels
+
+activityLabels <- read.table("activity_labels.txt")
+yDataMerged[, 1] <- activityLabels[yDataMerged[, 1], 2]
+
+
+# Merge all data and write .txt file for averages
+
+absoluteData <- cbind(xDataMerged, yDataMerged, subjectDataMerged)
+averages_data <- ddply(absoluteData, .(Subject, Activity), function(x) colMeans(x[, 1:66]))
+
+write.table(averages_data, "averages_data.txt", row.name=FALSE)
